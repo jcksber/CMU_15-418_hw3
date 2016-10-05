3219f9c77509842c74868132b2011866e699f1e9
caiqifang <cai2053@gmail.com>
2016-10-04 20:07:52 -0400

    add name charlie to wireroute.cpp

--- code/wireroute.cpp
+++ code/wireroute.cpp
@@ -1,6 +1,6 @@
 /**
  * Parallel VLSI Wire Routing via OpenMP
- * Jack Kasbeer (jkasbeer), Name 2(andrew_id 2)
+ * Jack Kasbeer (jkasbeer), Name 2: Charlie Cai (qcai)
  */
 
 #include "wireroute.h"
@@ -62,7 +62,7 @@ int main(int argc, const char *argv[])
 
   auto init_start = Clock::now();
   double init_time = 0;
- 
+
   _argc = argc - 1;
   _argv = argv + 1;
 
@@ -82,7 +82,7 @@ int main(int argc, const char *argv[])
     show_help(argv[0]);
     return 1;
   }
-  
+
   printf("Number of threads: %d\n", num_of_threads);
   printf("Probability parameter for simulated annealing: %lf.\n", SA_prob);
   printf("Number of simulated anneling iterations: %d\n", SA_iters);
@@ -94,7 +94,7 @@ int main(int argc, const char *argv[])
     printf("Unable to open file: %s.\n", input_filename);
     return 1;
   }
-  
+
   int dim_x, dim_y;
   int num_of_wires;
 
@@ -120,7 +120,7 @@ int main(int argc, const char *argv[])
   double compute_time = 0;
 #ifdef RUN_MIC /* Use RUN_MIC to distinguish between the target of compilation */
 
-  /* This pragma means we want the code in the following block be executed in 
+  /* This pragma means we want the code in the following block be executed in
    * Xeon Phi.
    */
 #pragma offload target(mic) \
@@ -136,7 +136,7 @@ int main(int argc, const char *argv[])
 
   compute_time += duration_cast<dsec>(Clock::now() - compute_start).count();
   printf("Computation Time: %lf.\n", compute_time);
-  
+
   /* Write wires and costs to files */
 
   return 0;
